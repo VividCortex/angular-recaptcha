3ae66b762d0209ac9bf5d0be9a7a929978198b39
Ismael Ambrosi <ismaambrosi@gmail.com>
2015-08-26 13:05:45 -0300

    [release]

--- release/angular-recaptcha.js
+++ release/angular-recaptcha.js
@@ -1,5 +1,5 @@
 /**
- * angular-recaptcha build:2015-06-22 
+ * angular-recaptcha build:2015-08-26 
  * https://github.com/vividcortex/angular-recaptcha 
  * Copyright (c) 2015 VividCortex 
 **/
@@ -133,6 +133,7 @@
 
                 scope.widgetId = null;
 
+                var sessionTimeout;
                 var removeCreationListener = scope.$watch('key', function (key) {
                     if (!key) {
                         return;
@@ -154,7 +155,7 @@
                         });
 
                         // captcha session lasts 2 mins after set.
-                        $timeout(function (){
+                        sessionTimeout = $timeout(function (){
                             if(ctrl){
                                 ctrl.$setValidity('recaptcha',false);
                             }
@@ -178,13 +179,26 @@
                         scope.widgetId = widgetId;
                         scope.onCreate({widgetId: widgetId});
 
-                        scope.$on('$destroy', cleanup);
+                        scope.$on('$destroy', destroy);
 
                     });
 
                     // Remove this listener to avoid creating the widget more than once.
                     removeCreationListener();
                 });
+
+                function destroy() {
+                  if (ctrl) {
+                    // reset the validity of the form if we were removed
+                    ctrl.$setValidity('recaptcha', null);
+                  }
+                  if (sessionTimeout) {
+                    // don't trigger the session timeout if we are no longer active
+                    $timeout.cancel(sessionTimeout);
+                    sessionTimeout = null;
+                  }
+                  cleanup();
+                }
 
                 function cleanup(){
                   // removes elements reCaptcha added.
