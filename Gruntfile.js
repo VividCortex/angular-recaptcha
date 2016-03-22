17bb81baa2936c82297dbdb02882b1b983356261
Ismael Ambrosi <ismaambrosi@gmail.com>
2016-03-22 16:36:03 -0300

    Configure grunt to use Chrome as test browser

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,7 +49,7 @@ module.exports = function (grunt) {
             unit:
             {
                 configFile: 'karma.conf.js',
-                browsers: ['PhantomJS'],
+                browsers: ['Chrome'],
                 singleRun: true
             }
         },
